a6814f753633fef6e67574afc1deb662b60ed6d3
Akash Jaiswal <akashjaiswal3846@gmail.com>
2021-08-02 19:11:44 +0530

    Connected to Atlas Database

--- server.js
+++ server.js
@@ -4,16 +4,29 @@
 const express = require('express')
 const bodyParser= require('body-parser')
 const app = express()
+const connectionString = "mongodb+srv://tanu:dbtanu03@cluster0.o4rgz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const MongoClient = require('mongodb').MongoClient
 
-// Make sure you place body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({ extended: true }))
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('Project0')
+    const quotesCollection = db.collection('quotes')
 
-app.listen(3000, function() {
-  console.log('listening on 3000')
-})
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
-})
-app.post('/quotes', (req, res) => {
-  console.log(req.body)
-})
+    // Make sure you place body-parser before your CRUD handlers!
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.listen(3000, function() {
+      console.log('listening on 3000')
+    })
+    app.get('/', function(req, res) {
+      res.sendFile(__dirname + '/index.html')
+    })
+    app.post('/quotes', (req, res) => {
+      quotesCollection.insertOne(req.body)
+        .then(result => {
+          console.log(result)
+        })
+        .catch(error => console.error(error))
+    })
+  })
